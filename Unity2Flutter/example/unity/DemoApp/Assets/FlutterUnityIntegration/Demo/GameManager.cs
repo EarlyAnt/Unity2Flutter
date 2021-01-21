35a6224bbc7486da17fee09d3a238ee27fed563f
EarlyAnt <54763755@qq.com>
2021-01-21 12:26:52 +0800

    1.将跟蓝度一起调通的，unity可以作为library在flutter中运行，且两者可以互发消息的成果，备份到新建的git库中。 2.将自己调通的，unity可以作为library在android中运行的成果，备份到新建的git库中。

---
+++ Unity2Flutter/example/unity/DemoApp/Assets/FlutterUnityIntegration/Demo/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text messageBox;
+    private UnityMessageManager unityMessageManager;
+
+    private void Awake()
+    {
+        GameObject go = new GameObject("UnityMessageManager");
+        DontDestroyOnLoad(go);
+        this.unityMessageManager = go.AddComponent<UnityMessageManager>();
+        this.unityMessageManager.SceneLoaded += this.OnSceneLoaded;
+        this.unityMessageManager.OnMessageReceive += this.OnReceiveMessage;
+    }
+
+    private void Start()
+    {
+    }
+
+    private void OnDestroy()
+    {
+        this.unityMessageManager.SceneLoaded += this.OnSceneLoaded;
+        this.unityMessageManager.OnMessageReceive -= this.OnReceiveMessage;
+    }
+
+    private void Update()
+    {
+    }
+
+    void OnSceneLoaded()
+    {
+        Debug.Log("<><GameManager.OnSceneLoaded>");
+        this.unityMessageManager.SendMessageToFlutter("ha ha, unity loaded ! ! !");
+    }
+
+    private void OnReceiveMessage(string message)
+    {
+        Debug.LogFormat("<><GameManager.OnReceiveMessage>message: {0}", message);
+        this.messageBox.text = message;
+    }
+}
